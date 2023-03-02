c4b83aa39663e9170f3b3e9761ae6066b7a46dc3
oidagsdhsagdhas44 <tornike.buchukuri@gau.edu.ge>
2023-03-02 14:09:52 +0400

    module4 loops final

--- src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
@@ -2,6 +2,11 @@ package school.mjc.stage0.loops.finalTask;
 
 public class HalfPyramid {
     public void printHalfPyramid(int cathetusLength) {
+        int whiteSpaceRepeat = cathetusLength-1;
+        for(int i=1; i<=cathetusLength; i++){
+            System.out.println(" ".repeat(whiteSpaceRepeat)+"*".repeat(i));
+            whiteSpaceRepeat--;
+        }
 
     }
 
--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,10 +2,22 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
-
+        int whiteSpaceRepeat = cathetusLength-1;
+        String prevNums = "";
+        for(int i=1; i<=cathetusLength; i++){
+            if(prevNums==""){
+                System.out.println(" ".repeat(whiteSpaceRepeat)+i+" ".repeat(whiteSpaceRepeat));
+                prevNums="1";
+            }
+            else{
+                System.out.println(" ".repeat(whiteSpaceRepeat)+i+prevNums+i+" ".repeat(whiteSpaceRepeat));
+                prevNums = i+prevNums+i;
+            }
+            whiteSpaceRepeat--;
+        }
     }
 
     public static void main(String[] args) {
-        new Pyramid().printPyramid(7);
+        new Pyramid().printPyramid(4);
     }
 }
